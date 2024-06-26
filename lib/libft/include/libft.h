5ff2a6b8f2095ceb372fe6a83ce3f7dde63ae994
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-06-26 13:55:41 -0300

    free trashman much simpler

--- lib/libft/include/libft.h
+++ lib/libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 11:14:54 by josfelip          #+#    #+#             */
-/*   Updated: 2024/06/26 13:38:55 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/06/26 13:52:20 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,6 @@ void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 void	ft_collect_mem(void *content);
-void	ft_free_trashman(t_list **lst_memory);
-t_list	**ft_get_mem_address(void);
+void	ft_free_trashman();
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:11:26 by josfelip          #+#    #+#             */
-/*   Updated: 2024/06/26 13:43:25 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/06/26 13:51:39 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ int	main(void)
 	str = ft_strdup("Hello World!\n");
 	ft_collect_mem(str);
 	new_function(str);
-	ft_free_trashman(ft_get_mem_address());
+	ft_free_trashman();
 	return (0);
 }
